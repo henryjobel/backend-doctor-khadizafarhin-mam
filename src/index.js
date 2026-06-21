00079f0c3524c7db6b1d2bff3151fb0275a91785
unknown <jhobelsaha@gmail.com>
2026-06-21 13:59:38 +0600

    Allow Vercel frontend origin

--- src/index.js
+++ src/index.js
@@ -26,7 +26,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "http://localhost:5174",
-  "http://127.0.0.1:5174"
+  "http://127.0.0.1:5174",
+  "https://dr-kazi-khadeza-farhin-client-site.vercel.app"
 ].filter(Boolean);
 
 app.use(
